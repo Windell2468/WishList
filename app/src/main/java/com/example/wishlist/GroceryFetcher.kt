93f5f3ec0d3d524b1bad55cdf12f4350fe6c589a
Jerry A Rogers <117837770+Windell2468@users.noreply.github.com>
2024-02-19 16:54:33 -0500

    This is the codes for project 2

--- app/src/main/java/com/example/wishlist/GroceryFetcher.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wishlist
-
-class GroceryFetcher {
-    companion object {
-        val Name = listOf("Apples","Pizzas","Oranges","Breads","Pies","Burgers","Fries","Hot Dogs","Pototes","Fried Chickens")
-        val Price = "How much is it for these items!"
-        val URL = ""
-    }
-}
--- app/src/main/java/com/example/wishlist/GroceryWishlistItems.kt
+++ app/src/main/java/com/example/wishlist/GroceryWishlistItems.kt
@@ -1,6 +1,6 @@
 package com.example.wishlist
 
-class Grocery (
+class GroceryWishlistItems (
     val Name: String,
     val Price: String,
     val URL: String) {
